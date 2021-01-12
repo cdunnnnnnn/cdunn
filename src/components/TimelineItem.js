382aba70c022161088c403ed9bbd70b47c791d3d
Chris Dunn <cdunn@manscaped.com>
2021-01-11 22:26:00 -0500

    🔗 adds timeline item urls

--- src/components/TimelineItem.js
+++ src/components/TimelineItem.js
@@ -4,7 +4,16 @@ export default function TimelineItem({ data }) {
   return (
     <div className="timeline__item">
       <span className="timeline__item-year">{data.year}</span>
-      <h2 className="timeline__item-title">{data.title}</h2>
+      <h2 className="timeline__item-title">
+        <a
+          className="hover:underline"
+          href={data.url}
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+        >
+          {data.title}
+        </a>
+      </h2>
       <p>{data.text}</p>
     </div>
   )
--- src/data/timeline.js
+++ src/data/timeline.js
@@ -4,17 +4,20 @@ module.exports = timeline_data = [
     text:
       "fullstack development across multiple publications/products with the CE1 scrum/agile shared services team",
     year: "2013",
+    url: "https://www.crain.com/",
   },
   {
     title: "Avocado Green Mattress",
     text:
       "fullstack development on the Avo engineering team with Elixir/Phoenix and React",
     year: "2017",
+    url: "https://www.avocadogreenmattress.com/",
   },
   {
     title: "MANSCAPED",
     text:
       "senior frontend developer role on the MANSCAPED team with Gatsby and React",
     year: "2020",
+    url: "https://www.manscaped.com/",
   },
 ]
